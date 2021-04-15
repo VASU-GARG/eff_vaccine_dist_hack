ca9c36f898852e60fbd8bca7f9f51c0a794e6988
VASU GARG <vasu.garg2020@vitstudent.ac.in>
2021-04-15 19:12:54 +0530

    First Commit

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+function display()
+{ 
+    alert("You have successfully registered for Covid 19 vaccine.\nWe have sent an appointment letter to your e-mail. Don't forget to bring it with you at the centre.\nWishing you a happy and healthy life.");
+    document.getElementById("form").reset();
+}
